7620abc25cec5be0d217441e283f533edc91f2eb
dannsgo <dannsgo@gmail.com>
2022-02-23 23:40:10 +0900

    tabMenu

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 // input > date
-const navDate = document.getElementById('nav_date');
-navDate.value = new Date().toISOString().substring(0, 10);
+document.getElementById('nav_date').value = new Date().toISOString().substring(0, 10);
+// tab menu
+const tabMenu = document.querySelectorAll('tab_btn');
+for(let i = 0; i < tabMenu.length; i++) {
+    tabMenu[i].getElementsByClassName('tab_btns').addEventListener('click', function(changeMenu){
+        changeMenu.preventDefault();
+        for(let j = 0; j < tabMenu.length; j++){
+            tabMenu[j].classList.remove('tab_menus');
+        }
+        this.parentNode.classList.add('tab_menus');
+    });
+}
 
 // Canvas
 const canvas = document.getElementById("boneCanvas");
