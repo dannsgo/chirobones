288dd22863fd6cf22f618a1a37dc96064b7a74e8
dannsgo <dannsgo@gmail.com>
2022-02-23 16:18:34 +0900

    input date 수정

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // input > date
-document.getElementById('nav_date').value = new Date(). toISOString().substring(0, 10);
+const navDate = document.getElementById('nav_date');
+navDate.value = new Date().toISOString().substring(0, 10);
 
 // Canvas
 const canvas = document.getElementById("boneCanvas");
 // Pixel Control == 2D
 const ctx = canvas.getContext("2d");
 
-canvas.width = ;
-canvas.height = ;
+// canvas.width = ;
+// canvas.height = ;
 
-// 선 색상
-ctx.strokeStyle = "";
+// // 선 색상
+// ctx.strokeStyle = "";
