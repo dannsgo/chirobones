41b9983abdf800840c6b7ff16e266ed53b2f9784
dannsgo <dannsgo@gmail.com>
2022-02-23 11:43:39 +0900

    폰트 설정

---
+++ app.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("boneCanvas");
+// Pixel Control == 2D
+const ctx = canvas.getContext("2d");
+
+canvas.width = ;
+canvas.height = ;
+
+// 선 색상
+ctx.strokeStyle = "";
