75dc0684083b03b39500364d1f66eb6013c45e63
dannsgo <dannsgo@gmail.com>
2022-03-07 23:05:10 +0900

    canvas api

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
-// input > date
-document.getElementById('nav_date').value = new Date()
-  .toISOString()
-  .substring(0, 10);
-// tab menu
-const tabList = document.querySelectorAll('.tab_menu .tab_btn li');
+// ! input > date
+let date = document.getElementById("nav_date");
+date.value = new Date().toISOString().substring(0, 10);
+
+// ! tab menu
+const tabList = document.querySelectorAll(".tab_menu .tab_btn li");
 for (let i = 0; i < tabList.length; i++) {
-  tabList[i]
-    .querySelector('.tab_btns')
-    .addEventListener('click', function (changeTab) {
-      changeTab.preventDefault();
-      for (let j = 0; j < tabList.length; j++) {
-        tabList[j].classList.remove('tab_menus');
-      }
-      this.parentNode.classList.add('tab_menus');
+    tabList[i].querySelector(".tab_btns").addEventListener("click", function (changeTab) {
+        changeTab.preventDefault();
+        for (let j = 0; j < tabList.length; j++) {
+            tabList[j].classList.remove("tab_menus");
+        }
+        this.parentNode.classList.add("tab_menus");
     });
 }
 
-// Canvas
-const canvas = document.getElementById('boneCanvas');
-// Pixel Control == 2D
-const ctx = canvas.getContext('2d');
+// ! Canvas
+const canvas = document.getElementById("boneCanvas");
+const ctx = canvas.getContext("2d");
 
-// canvas.width = ;
-// canvas.height = ;
+// start__coordinate
+function coordinate(event) {
+    let x = event.offsetX; // X-coordinate
+    let y = event.offsetY; // y-coordinate\
+    console.log(x, y);
+}
 
-// // 선 색상
+if (canvas) {
+    canvas.addEventListener("mousemove", coordinate);
+}
+
+// coordinate__end
+
+// start__image uploader
+const reader = new FileReader();
+const img = new Image();
+
+const uploadImage = (upload) => {
+    reader.onload = () => {
+        img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+        };
+        img.src = reader.result;
+    };
+    reader.readAsDataURL(upload.target.files[0]);
+};
+const imageLeader = document.getElementById("uploader");
+imageLeader.addEventListener("change", uploadImage);
+// image uploader__end
+// image download
+
+// 선 색상
 // ctx.strokeStyle = "";
